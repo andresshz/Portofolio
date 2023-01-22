d8d4dd741851f873c3695b01169a23f2156d16ab
Andrés Fernández <zhsshz17@gmail.com>
2023-01-21 23:01:12 -0600

    Actualización de data: Apartado NodeJS

--- src/data/data.js
+++ src/data/data.js
@@ -2,13 +2,13 @@ export default [
     {
         "nombre": "Laravel",
         "path": "/assets/laravel.svg",
-        "descripcion":"He desarrollado aplicaciones web con laravel 8 desde mi instancia como pasante en la institución de Dirección de Salud Ambiental de El Salvador, desarrollando aplicaciones con el patrón MVC, realizando sistema de authenticación (registro / login) con rutas protegidas, creación de reportes pdf, utlizando MySql para mis bases de datos y Eloquent un ORM que me proporciona laravel para interactuar con mi base de datos.",
+        "descripcion":"He desarrollado aplicaciones web con laravel 8 desde mi instancia como pasante en la institución de Dirección de Salud Ambiental de El Salvador, desarrollando aplicaciones con el patrón MVC, realizando sistema de authenticación (registro / login) con su middelware, creación de reportes pdf, utlizando MySql para mis bases de datos y Eloquent un ORM que me proporciona laravel para interactuar con mi base de datos.",
     },
 
     {
         "nombre": "Nodejs",
         "path": "/assets/nodejs.png",
-        "descripcion":"He creado apis con nodejs para que sean consumidas apoyandome del framework express a la crear mi servidor y definir sus rutas donde se estaran enviando las peticiones por el usuario,  obtuve mis conocimientos sobre Node.js"
+        "descripcion":"En nodejs me he desempeñado en la creación de REST APIS apoyandome del framework express para crear mi servidor y definir mis rutas con las  que contara mi api para recibir las peticiones y los datos que retornara al cliente. También realizando sistema te autenticación, creando sistemas para control de Inventario, y ocupando como bases de datos en mis proyectos Mongo Atlas como noSql y MySql, Mis conocimientos de NodeJs los adquiri mediante el curso de Nicolas Schurmann en Udemy."
     },
 
     {
